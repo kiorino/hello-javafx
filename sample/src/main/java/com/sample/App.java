ff69861e87521194439b68e9c103d839858c2c08
kiorino <escapeaddress@gmail.com>
2019-01-14 21:18:42 +0900

    add myButton

--- sample/src/main/java/com/sample/App.java
+++ sample/src/main/java/com/sample/App.java
@@ -5,36 +5,50 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
  
 public class App extends Application {
-    Label label;
-    Button button;
-    TextField textField;
  
     @Override
     public void start(Stage stage) throws Exception {
-        label = new Label("入力された文字:");
-        textField = new TextField();
-        button = new Button("OK");
+        Label label = new Label("入力された文字:");
+        TextField textField = new TextField();
+        Button OkButton = new Button("OK");
+        Button NgButton = new Button("NG");
+        // //buttonクリック時のアクション
+        // OkButton.setOnAction(new EventHandler<ActionEvent>() {
+        //     @Override
+        //     public void handle(ActionEvent e) {
+        //         String typed_msg = "入力された文字:" + textField.getText();
+        //         label.setText(typed_msg);
+        //     }
+        // });
+		// button2 = new Buttom("NG");
         //buttonクリック時のアクション
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                String typed_msg = "入力された文字:" + textField.getText();
-                label.setText(typed_msg);
-            }
-        });
+        // button2.setOnAction(new EventHandler<ActionEvent>() {
+        //     @Override
+        //     public void handle(ActionEvent e) {
+        //         String typed_msg = "NG入力された文字:" + textField.getText();
+        //         label.setText(typed_msg);
+        //     }
+        //});
  
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(textField);
-        borderPane.setCenter(label);
-        borderPane.setBottom(button);
-        Scene scene = new Scene(borderPane, 240, 120);
+
+        // BorderPane borderPane = new BorderPane();
+        // borderPane.setTop(textField);
+        // borderPane.setCenter(label);
+        // borderPane.setBottom(OkButton);
+        // borderPane.setLeft(NgButton);
+		AnchorPane pane = new AnchorPane();
+		int num = 0;
+		MyButton button1 = new MyButton(pane, "button" + String.valueOf(num++), 10., 10.);
+		MyButton button2 = new MyButton(pane, "button" + String.valueOf(num++), 20., 20.);
+        Scene scene = new Scene(pane, 500, 500);
         stage.setScene(scene);
-        stage.setTitle("TestApp");
+        stage.setTitle("アプリケーション");
         stage.show();
     }
  
